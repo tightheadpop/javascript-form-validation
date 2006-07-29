4b12bbaa639ba41f7b08ffbd42b266ba6d915b90
matthew.a.frank <matthew.a.frank@1d203c55-ac19-0410-a171-f304b91d6a96>
2006-07-29 03:31:32 +0000

    initial commit; refactoring + test writing + prototype-ification in progress

---
+++ trunk/test/settings.js
@@ -0,0 +1,4 @@
+document.write('<script src="../../jsunit/app/jsUnitCore.js"></script>');
+document.write('<script src="../prototype.js"></script>');
+document.write('<script src="../validation.js"></script>');
+var path = "../validation/test/";
